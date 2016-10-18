0126b7915e18394140e7ea9875c4585e468e2c63
daylight <blogr@uncoveryourlife.com>
2016-10-18 19:49:22 -0400

    adding in files from previous commit - forgot to add

---
+++ CYaPass/UserPath.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+
+
+namespace CYaPass
+{
+    class UserPath
+    {
+        public List<Point> allPoints = new List<Point>();
+        public HashSet<Segment> allSegments = new HashSet<Segment>();
+        public HashSet<int> postPointSet = new HashSet<int>();
+        private Point currentPoint;
+        public int PointValue;
+
+        public void append(Point currentPoint, int postValue)
+        {
+            this.currentPoint = currentPoint;
+           
+            if (allPoints.Count == 0)
+            {
+                PointValue += postValue;
+            }
+            if (allPoints.Count >= 1)
+            {
+                int currentSegmentCount = allSegments.Count;
+                allSegments.Add(new Segment(allPoints[allPoints.Count - 1], currentPoint));
+                
+                if (currentSegmentCount < allSegments.Count)
+                {
+                    // a new segment was added so you add more to the PointValue
+                    PointValue += postValue;
+                }
+            }
+            allPoints.Add(currentPoint);
+
+        }
+    }
+}
